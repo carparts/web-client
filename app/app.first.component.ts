c1c8c99fee86f4f0f82ca9b579aeee3e00743f3d
masteringjs <kestutis.dragunas@gmail.com>
2016-09-17 19:51:05 +0300

    Test

---
+++ app/app.first.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>First component</h1>'
+})
+export class FirstComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FirstComponent }   from './app.first.component';
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ FirstComponent ],
+  bootstrap:    [ FirstComponent ]
+})
+export class AppModule { }
