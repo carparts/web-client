6bf617aed39a57fab0a6131437edd27a89d3f97c
masteringjs <kestutis.dragunas@gmail.com>
2016-11-07 18:31:09 +0200

    dynamic fields instead of static

--- app/car-parts/search/search.component.ts
+++ app/car-parts/search/search.component.ts
@@ -6,28 +6,33 @@ import {AutoComplete, Password} from 'primeng/primeng';
 @Component({
   selector: 'car-search',
   template: `<h1>Search component</h1>
-<p-autoComplete [(ngModel)]="fields['make']" [suggestions]="filteredData['make']" (completeMethod)="search($event, 'make')" [dropdown]="true" (onDropdownClick)="handleDropdownClick($event, 'make')" [size]="30"
-    [minLength]="1" placeholder="make">
-</p-autoComplete>
-<p-autoComplete [(ngModel)]="fields['model']" [suggestions]="filteredData['model']" (completeMethod)="search($event, 'model')" [dropdown]="true" (onDropdownClick)="handleDropdownClick($event, 'model')" [size]="30"
-    [minLength]="1" placeholder="model">
-</p-autoComplete>
-<p-autoComplete [(ngModel)]="fields['year']" [suggestions]="filteredData['year']" (completeMethod)="search($event, 'year')" (onDropdownClick)="handleDropdownClick($event, 'year')" [dropdown]="true" [size]="30"
-    [minLength]="1" placeholder="year">
-</p-autoComplete>`,
+  <span *ngFor="let field of fieldsArray">
+    <p-autoComplete [(ngModel)]="fields[field]" [suggestions]="filteredData[field]" (completeMethod)="search($event, field)" [dropdown]="true" (onDropdownClick)="handleDropdownClick($event, field)" [size]="30"
+      [minLength]="1" placeholder="{{field}}"></p-autoComplete>
+  </span>
+`,
   providers: [SearchService]
 })
 
 export class SearchComponent {
     fields: {[id:string] : string}
-    filteredData : {[id:string]: any[]};
+    filteredData : {[id:string]: string[]};
+    fieldsArray : string[];
 
   constructor(private searchService: SearchService) {
-    this.fields = {"model":"", "make":"", "year":""};
-    this.filteredData = {"model":[], "make":[], "year":[]};
+    this.fieldsArray = ["make","model","year"];
+    this.fields = {};
+    this.filteredData = {};
+    this.fieldsArray.map( field => {
+        this.fields[field] = "";
+        this.filteredData[field] = [];
+    });
    }
 
   search(event, field) {
+    this.fieldsArray.map( field => {
+        this.filteredData[field] = [];
+    });
     this.searchService.getSearchResults().then(results => {
       let filteredRows = results.filter(
         row => {
@@ -61,38 +66,3 @@ export class SearchComponent {
         }, 100)
     }
 }
-
- export class SearchComponent2 {
-    country: any;
-    
-    countries: any[];
-        
-    filteredCountriesSingle: any[];
-    
-    filteredCountriesMultiple: any[];
-    
-    brands: string[] = ['Audi','BMW','Fiat','Ford','Honda','Jaguar','Mercedes','Renault','Volvo','VW'];
-    
-    filteredBrands: any[];
-    
-    brand: string;
-        
-    filterBrands(event) {
-        this.filteredBrands = [];
-        for(let i = 0; i < this.brands.length; i++) {
-            let brand = this.brands[i];
-            if(brand.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
-                this.filteredBrands.push(brand);
-            }
-        }
-    }
-    
-    handleDropdownClick() {
-        this.filteredBrands = [];
-        
-        //mimic remote call
-        setTimeout(() => {
-            this.filteredBrands = this.brands;
-        }, 100)
-    }
- }
